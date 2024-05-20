b3e034a5dc93daa6a3f48bc9539db9e87922b3a7
Caio Malheiros <md.caio@yahoo.com.br>
2024-05-20 13:54:59 -0300

    Criando as funções do projeto

--- app.py
+++ app.py
@@ -1,11 +1,71 @@
-print('Sabor Express \n')
+
+# importando bibliotecas
+import os
+
+def exibir_nome_do_programa(): 
+    print(""" 
+    
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+      """)
+
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante \n')
+    print('2. Listar Restaurante \n')
+    print('3. Ativar Restaurante \n')
+    print('4. Sair \n')
 
 
-print('1. Cadastrar Restaurante \n')
-print('2. Listar Restaurante \n')
-print('3. Ativar Restaurante \n')
-print('4. Sair \n')
+def opcao_invalida():
+    print('Opção inválida\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
+def escolher_opcao():
+    try:
+        # Declarando variaveis
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # print('Você escolheu a opção', opcao_escolhida)
+        if opcao_escolhida == 1:
+            print('Cadastrar Restaurante')
+
+        elif opcao_escolhida == 2:
+            print('Listar Restaurante')
+
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+
+        elif opcao_escolhida == 4:
+            finalizar_app()    
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+        
+# Utilizando interpolação no python
+#  print(f'Você escolheu a opção {opcao_escolhida}')
+
+# Criando funções em python
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app')
 
 
-# Declarando variaveis
-opcao_escolhida = input('Escolha uma opção: ')
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+     
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
