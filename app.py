2977b19a4d98374f0b81c400bdf80d2149fe9be6
Caio Malheiros <md.caio@yahoo.com.br>
2024-05-20 08:47:25 -0300

    Curso de Python Alura

---
+++ app.py
@@ -0,0 +1,11 @@
+print('Sabor Express \n')
+
+
+print('1. Cadastrar Restaurante \n')
+print('2. Listar Restaurante \n')
+print('3. Ativar Restaurante \n')
+print('4. Sair \n')
+
+
+# Declarando variaveis
+opcao_escolhida = input('Escolha uma opção: ')
